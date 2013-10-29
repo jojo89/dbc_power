5a580ddd3e6e0372888a95d411aae186bda2c235
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-29 09:55:15 -0500

    swap

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,34 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-students = DBC::User.all
 
-students.each do |student|
-   Student.create!( uid: student.id,
-                    name: student.name,
-                    email: student.email,
-                    bio: student.bio,
-                    cohort_id: student.cohort_id,
-                    cohort_name: DBC::Cohort.find(student.cohort_id).name,
-                    github: student.profile[:github],
-                    quora: student.profile[:quora],
-                    twitter: student.profile[:twitter],
-                    facebook: student.profile[:facebook],
-                    linked_in: student.profile[:linked_in],
-                    about: student.profile[:about]
-                  )
+students1 = DBC::Cohort.find(26).students
+students2 = DBC::Cohort.find(28).students
+students3 = DBC::Cohort.find(30).students
+students4 = DBC::Cohort.find(32).students
+
+stdnts= [students1, students2, students3, students4]
+stdnts.each do |array|
+    array.each do |student|
+      Student.create!(
+        uid: student.id,
+        about: student.profile[:about],
+        bio: student.bio,
+        cohort_id: student.cohort_id,
+        cohort_name: DBC::Cohort.find(student.cohort_id).name,
+        email: student.email,
+        facebook: student.profile[:facebook],
+        github: student.profile[:github],
+        linked_in: student.profile[:linked_in],
+        name: student.name,
+        quora: student.profile[:quora],
+        twitter: student.profile[:twitter])
+    end
 end
+#
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Examples:
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# This file should contain all the record creation needed to seed the database with its default values.
